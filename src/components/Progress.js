b50830285634ac598f46630bf6b6afe48e97f4c4
Rashmi-Salunkhe <158038788+Rashmi-Salunkhe@users.noreply.github.com>
2024-09-24 11:08:18 +0530

    Update Progress.js

--- src/components/Progress.js
+++ src/components/Progress.js
@@ -1,41 +1,55 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class Progress extends Component {
   constructor(props) {
-    super(props)
-  
+    super(props);
     this.state = {
-       progress:0
-    }
+      progress: 0,
+      barWidth: 200 // total width in pixels
+    };
   }
-  startProcess(){
-    this.interval=setInterval(()=>{
-        this.setState((prevstate)=>({
-            progress:prevstate.progress+10
-        }),()=>{
-            if(this.state.progress>=100){
-                clearInterval(this.interval);
-            }
-        })
 
-
-    },1000)
+  startProcess() {
+    this.interval = setInterval(() => {
+      this.setState(
+        (prevState) => ({
+          progress: prevState.progress + 10
+        }),
+        () => {
+          if (this.state.progress >= 100) {
+            clearInterval(this.interval);
+          }
+        }
+      );
+    }, 1000);
   }
- 
+
   componentDidMount() {
     this.startProcess();
   }
+
   componentWillUnmount() {
     clearInterval(this.interval);
   }
+
   render() {
+    const currentWidth = (this.state.progress / 100) * this.state.barWidth;
+
     return (
       <div>
-        <div id="barOuter" style={{width:"200px",height:"30px",backgroundColor:"lightgray"}}>
-        <div id="barInner" style={{width:`${this.state.progress}%`,height:"30px",backgroundColor:"blue"}}></div></div><p>{`${this.state.progress}%`}</p>
+        <div
+          id="barOuter"
+          style={{ width: `${this.state.barWidth}px`, height: '30px', backgroundColor: 'lightgray' }}
+        >
+          <div
+            id="barInner"
+            style={{ width: `${currentWidth}px`, height: '30px', backgroundColor: 'blue' }}
+          ></div>
+        </div>
+        <p>{`${this.state.progress}%`}</p>
       </div>
-    )
+    );
   }
 }
 
-export default Progress
+export default Progress;
